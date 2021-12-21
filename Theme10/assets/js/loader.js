451601af765db3309ca4ede8cb46bce254ffedf9
witsuu <witsudi217@gmail.com>
2021-12-21 21:18:58 +0700

    update js

--- Theme10/assets/js/loader.js
+++ Theme10/assets/js/loader.js
@@ -9,13 +9,11 @@
 // };
 
 $(document).on("readystatechange", () => {
-  $(document).on("readystatechange", () => {
-    if (document.readyState !== "complete") {
-      document.querySelector(".main-app").style.display = "none";
-      document.querySelector(".loader-wrapper").style.display = "block";
-    } else {
-      document.querySelector(".main-app").style.display = "block";
-      document.querySelector(".loader-wrapper").style.display = "none";
-    }
-  });
+  if (document.readyState !== "complete") {
+    document.querySelector(".main-app").style.visibility = "hidden";
+    document.querySelector(".loader-wrapper").style.visibility = "visible";
+  } else {
+    document.querySelector(".main-app").style.visibility = "visible";
+    document.querySelector(".loader-wrapper").style.visibility = "hidden";
+  }
 });
