23df427d3f6b86aa05ccc4b6ff7ce360647556e3
witsuu <witsudi217@gmail.com>
2021-12-21 09:55:38 +0700

    revisi

--- Theme11/assets/js/app.js
+++ Theme11/assets/js/app.js
@@ -13,10 +13,13 @@ $(document).ready(() => {
   //   controls: true,
   // });
 
-  $(".gallery").magnificPopup({
+  $(".zoom-gallery").magnificPopup({
     delegate: "a",
     type: "image",
-    mainClass: "mfp-with-zoom",
+    mainClass: "mfp-with-zoom mfp-img-mobile",
+    gallery: {
+      enabled: true,
+    },
     zoom: {
       enabled: true,
       easing: "ease-in-out",
