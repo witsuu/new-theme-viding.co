038e3c61ba4ae3ffb36711e69a468d3aa664206e
witsuu <witsudi217@gmail.com>
2021-12-21 12:36:28 +0700

    set font size in html tag

--- Theme10/assets/js/loader.js
+++ Theme10/assets/js/loader.js
@@ -11,11 +11,11 @@
 $(document).on("readystatechange", () => {
   $(document).on("readystatechange", () => {
     if (document.readyState !== "complete") {
-      document.querySelector(".main-app").style.visibility = "hidden";
-      document.querySelector(".loader-wrapper").style.visibility = "visible";
+      document.querySelector(".main-app").style.display = "none";
+      document.querySelector(".loader-wrapper").style.display = "block";
     } else {
-      document.querySelector(".main-app").style.visibility = "visible";
-      document.querySelector(".loader-wrapper").style.visibility = "hidden";
+      document.querySelector(".main-app").style.display = "block";
+      document.querySelector(".loader-wrapper").style.display = "none";
     }
   });
 });
