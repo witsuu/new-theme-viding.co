b4377df74dcc412d0a306319e81d01854c9bd829
witsuu <witsudi217@gmail.com>
2022-01-15 16:45:11 +0700

    fixed enveloped

--- Theme09/assets/js/app.js
+++ Theme09/assets/js/app.js
@@ -8,26 +8,6 @@ $(document).ready(() => {
   const date = $(".countdown").attr("date");
   Countdown(date);
 
-  //autoslider
-  // $("#slider").infiniteslide({
-  //   speed: 50,
-  //   direction: "up",
-  // });
-
-  const owl = $(".owl-carousel");
-  owl.owlCarousel({
-    items: 1,
-    loop: true,
-    dots: false,
-  });
-
-  $(".next").on("click", () => {
-    owl.trigger("next.owl.carousel");
-  });
-  $(".prev").on("click", () => {
-    owl.trigger("prev.owl.carousel");
-  });
-
   $("body").css("overflow-y", "hidden");
 
   $("#btn-envelope").on("click", function () {
@@ -36,5 +16,3 @@ $(document).ready(() => {
     $(".envelope-wrap").css("transform", "translateY(-100%)");
   });
 });
-
-AOS.refresh();
